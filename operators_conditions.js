7f6f12b0e438d95c8ccaf62d38e98393b523099e
heenathaseen <heenathaseen98@gmail.com>
2019-06-22 16:08:21 +0530

    js using operators and conditions

---
+++ operators_conditions.js
@@ -0,0 +1,39 @@
+console.log(6 + 9);
+console.log("heena" + "thaseen");
+
+console.log(9 - 2);
+console.log(8 * 2);
+console.log(9 / 3);
+
+var numOne = 1;
+var numTwo = 2;
+
+if (numOne === 2) {
+  console.log(numOne);
+} else {
+  console.log(numTwo);
+}
+
+if (numTwo !== 1) {
+  console.log(numTwo);
+}
+
+var userName = 'Heena Thaseen';
+if (userName === 'Heena Thaseen' ) {
+  console.log('Loggeg in User : ' + userName);
+} else {
+  console.log('No User');
+}
+
+var name = 'heena';
+console.log(name);
+if (name == 'heena') {
+  name += ' :)';
+  console.log(name);
+} else if (name == 'thaseen') {
+  name += ' :P';
+  console.log(name);
+} else {
+  name += ' :D';
+  console.log(name);
+}
